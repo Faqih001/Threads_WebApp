930bed166f02d41bb954ab799d9000939dda230b
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-24 23:48:28 +0300

    Add comment in getMessages function to clarify user ID retrieval from request parameters

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -80,6 +80,7 @@ async function sendMessage(req, res) {
 
 // Get Messages API endpoint (get all messages in a conversation) by conversationId API endpoint
 async function getMessages(req, res) {
+	// Other user id from the request params and user id from the request user object
 	const { otherUserId } = req.params;
 	const userId = req.user._id;
 	try {
