a4dc657d04e6d63a3ed9b444575ebfe08000ac12
Amirul Faqih <fakiiahmad001@gmail.com>
2025-03-01 23:37:22 +0300

    Add comment in getFeedPosts function to clarify user ID retrieval

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -178,8 +178,11 @@ const replyToPost = async (req, res) => {
 const getFeedPosts = async (req, res) => {
 	// Try to get all posts from users that the current user is following
 	try {
+		// Get the user id from the request user object
 		const userId = req.user._id;
 		const user = await User.findById(userId);
+		
+		//
 		if (!user) {
 			return res.status(404).json({ error: "User not found" });
 		}
