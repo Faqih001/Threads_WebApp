93bcf1c2add644790b1866b6ee78fa9e02b6656d
Amirul Faqih <fakiiahmad001@gmail.com>
2025-03-01 23:36:32 +0300

    Add comment in replyToPost function to clarify reply return logic

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -167,6 +167,7 @@ const replyToPost = async (req, res) => {
 		post.replies.push(reply);
 		await post.save();
 
+		// Return the reply
 		res.status(200).json(reply);
 	} catch (err) {
 		res.status(500).json({ error: err.message });
