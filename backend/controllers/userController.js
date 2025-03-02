d69277848d8c1f6696715ecc5ba45a90150e0a08
Amirul Faqih <fakiiahmad001@gmail.com>
2025-03-02 23:10:59 +0300

    Add comment in logoutUser function to clarify cookie clearing process

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -127,6 +127,7 @@ const loginUser = async (req, res) => {
 
 // Logout user (protected route) - clear cookie with jwt token
 const logoutUser = (req, res) => {
+	// try to clear cookie with jwt token
 	try {
 		res.cookie("jwt", "", { maxAge: 1 });
 		res.status(200).json({ message: "User logged out successfully" });
