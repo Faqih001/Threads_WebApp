1a05d1e2cf0c820d3c0c4624dee14c1f3ba5a8b5
Amirul Faqih <fakiiahmad001@gmail.com>
2025-03-03 22:59:46 +0300

    Add comment in followUnFollowUser function to indicate user not found error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -154,6 +154,7 @@ const followUnFollowUser = async (req, res) => {
 		if (id === req.user._id.toString())
 			return res.status(400).json({ error: "You cannot follow/unfollow yourself" });
 
+		// If user is not found, return error message "User not found"
 		if (!userToModify || !currentUser) return res.status(400).json({ error: "User not found" });
 
 		const isFollowing = currentUser.following.includes(id);
