adaee7ccf2fe1392c93937881ef3b4b279ffc3ec
Amirul Faqih <fakiiahmad001@gmail.com>
2025-03-07 23:29:21 +0300

    Add comment in server.js to indicate Cloudinary configuration for image uploads

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ job.start();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
+// Cloudinary configuration for image uploads
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
