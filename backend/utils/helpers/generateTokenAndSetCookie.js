76f471441725454e961a2db96d5cf61e56adbb38
Amirul Faqih <fakiiahmad001@gmail.com>
2025-03-07 23:27:50 +0300

    Add comment in generateTokenAndSetCookie.js to clarify JWT token generation process

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -2,10 +2,12 @@ import jwt from "jsonwebtoken";
 
 // Generate JWT token and set it in a cookie for the user
 const generateTokenAndSetCookie = (userId, res) => {
+	// Generate a JWT token with the user id and the JWT secret
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "15d",
 	});
 
+	//
 	res.cookie("jwt", token, {
 		httpOnly: true, // more secure
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
